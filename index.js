6e917f541ae380b9fa7585f8f69f5ed314fb509d
Favour Achara <acharafavour01@example.com>
2024-05-29 15:13:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+tailwind.config={
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                "logo": {
+                    700: "#e9c604",
+                    800: "#c2a501"
+                }
+                }
+            }
+        }
+}
